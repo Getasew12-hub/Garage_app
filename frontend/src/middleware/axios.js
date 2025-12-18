afa11db3875699e85229d429b7ac0ec87f8aaff9
Getasew Tewachew <getasewtewachew@gmail.com>
2025-12-18 13:14:44 -0800

    axois

--- frontend/src/middleware/axios.js
+++ frontend/src/middleware/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.MODE==='development'?'http://localhost:5000/api':'/api',
+    baseURL:import.meta.MODE==='development'?'http://localhost:5000/api':import.meta.env.VITE_BACKEN_URL +'/api/',
     withCredentials:true
 })
 
